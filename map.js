65f812bb245f215f67d92ade5aa4546a17c805d6
Nick Szydlowski <nick.szydlowski@sjsu.edu>
2023-10-20 00:11:17 -0700

    add markers per level

--- map.js
+++ map.js
@@ -1,8 +1,33 @@
-function docLoop (floors) {
+function buildMap (markers) {
+  map = L.map('map', {
+      crs: L.CRS.Simple,
+      minZoom: -1,
+  });
 
+  docLoop(floors, latLngBounds);
+  map.fitBounds(latLngBounds);
+  map.on('baselayerchange', function(e) {
+    currentBaseLayer=e.name;
+    onBaseChange();
+  });
+
+
+
+  //Click on the map to get coordinates - turn on for configuration
+  //Maybe add a button for this?
+  //map.on('click', onMapClick);
+
+}
+
+
+function docLoop (floors) {
   floors.forEach (function(svgDoc){
     loadDoc(svgDoc);
   });
+  layerControl = L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false,
+    }).addTo(map);
+  initMarkers();
 
 };
 
@@ -18,8 +43,12 @@ function XMLprocess(xml, svgDoc) {
       //opacity: 0.7,
       interactive: true
   });
-  mysvgOverlay.addTo(map);
+
+  if (svgDoc=="1.svg") { //start on floor 1
+    mysvgOverlay.addTo(map);
+  };
   layerControl.addBaseLayer(mysvgOverlay, svgDoc.split(".")[0]);
+  baseMaps[svgDoc.split(".")[0]]=mysvgOverlay;
 }
 
 
@@ -28,7 +57,7 @@ function loadDoc(svgDoc) {
  var xhttp = new XMLHttpRequest();
  xhttp.onreadystatechange = function() {
    if (this.readyState == 4 && this.status == 200) {
-   XMLprocess(this, svgDoc);
+   XMLprocess(this, svgDoc, latLngBounds);
    }
  };
  xhttp.open("GET", svgDoc, true);
@@ -37,20 +66,38 @@ function loadDoc(svgDoc) {
 }
 
 function onBaseChange(e) {
-  console.log('Base layer changed - this should change marker options');
+  deleteMarkers();
+  currentMarkers =[];
+  //levelMarkers.remove();
+  markers.forEach((element, index, array) => {
+    if (element.level == currentBaseLayer) {
+      var location = element.coordinates.split(',');
+      var thisMarker = L.marker(location).bindPopup('<h4>'+element.popupHead+'</h4>'+element.popupBody);
+      currentMarkers.push(thisMarker);
+    }
+  });
+  levelMarkers=L.layerGroup(currentMarkers);
+  map.removeControl(layerControl);
+  layerControl = L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false,
+    }).addTo(map);
+  layerControl.addOverlay(levelMarkers, "Markers");
 }
 
-var map = L.map('map', {
-    crs: L.CRS.Simple,
-    minZoom: -1,
-});
-var latLngBounds = L.latLngBounds([[0,0], [1000,1000]]);
-var floors = ['Floor_1_edit.svg', 'Floor_2_edit.svg'];
-var baseMaps = {};
-var overlayMaps = {};
-var layerControl = L.control.layers(baseMaps, overlayMaps).addTo(map);
-docLoop(floors);
-map.fitBounds(latLngBounds);
+function initMarkers() {
+  currentMarkers =[];
+  var levelMarkers;
+  markers.forEach((element, index, array) => {
+    if (element.level == currentBaseLayer) {
+      var location = element.coordinates.split(',');
+      var thisMarker = L.marker(location).bindPopup('<h4>'+element.popupHead+'</h4>'+element.popupBody);
+      currentMarkers.push(thisMarker);
+    }
+  });
+  levelMarkers=L.layerGroup(currentMarkers);
+  layerControl.addOverlay(levelMarkers, "Markers");
+}
+
 
 
 //keep this function as a way to get coordinates for items on the map
@@ -58,8 +105,36 @@ function onMapClick(e) {
     alert("You clicked the map at " + e.latlng);
 }
 
-//map.on('click', onMapClick);
+// var map is an instance of a Leaflet map
+// this function assumes you have added markers as GeoJSON to the map
+// it will return an array of all features currently shown in the
+// active bounding region.
+//for testing
 
-var marker = L.marker([116, 396]).addTo(map).bindPopup("<b>Campus Entrance</b><br>Enter here from SJSU Campus.");
+function deleteMarkers() {
+  var features = [];
+  map.eachLayer( function(layer) {
+    if(layer instanceof L.Marker) {
+        map.removeLayer(layer);
+    }
+  });
+}
 
-map.on('baselayerchange', onBaseChange);
+
+//end of function definitions
+var map;
+var layerControl;
+var floors = ['1.svg', '2.svg'];
+var latLngBounds = L.latLngBounds([[0,0], [1000,1000]]);
+var baseMaps = {};
+var overlayMaps = {};
+var markers
+var currentBaseLayer = 1;
+var currentMarkers = [];
+
+$(document).ready(function() {
+  $.getJSON( "markers.json", function(data) {
+    markers = data;
+    buildMap(markers, floors, latLngBounds);
+  });
+});
